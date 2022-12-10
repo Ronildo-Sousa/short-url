3bc1f36eb9cdbeef50e6cb2fa8fe49480a426700
Ronildo Sousa <sousaronildo400@gmail.com>
2022-12-09 22:38:13 -0300

    initiallizing frontend from Shortner

--- resources/views/livewire/shortner.blade.php
+++ resources/views/livewire/shortner.blade.php
@@ -1,9 +1,14 @@
-<div>
-   {{ $shortUrl }}
-   @error('customCode')
-      {{ $message }}
-   @enderror
-   @error('url')
-      {{ $message }}
-   @enderror
-</div>
+<section>
+    <div>
+        <input wire:model='url' type="url" placeholder="your url">
+        @error('url')
+            <small class="text-red-500">
+                {{ $message }}
+            </small>
+        @enderror
+    </div>
+    @error('customCode')
+        {{ $message }}
+    @enderror
+    <button wire:click='create' class="p-3 bg-blue-400">short</button>
+</section>
